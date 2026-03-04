b821341c5bcd8f5c864f0d4a4248b62ee610aef5
Koumolou <learningsin2024@gmail.com>
2026-03-04 03:41:22 +0000

    Signup route

--- project/signup.php
+++ project/signup.php
@@ -95,7 +95,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 }
 
                 mysqli_stmt_close($stm);
-                header("Location: Dashboard.php");
+                header("Location: dashboard.php");
                 exit;
             } else {
                 $errorpass = "Database insert error: " . mysqli_error($conn);
